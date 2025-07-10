db2d6862b7c9b31a37e967cb892a270e7a677125
Andrew Lenox <zmobie@gmail.com>
2025-07-09 20:39:56 -0400

    Remove Xcode build artifacts from version control
    - Remove old Xcode build/ directory and all compiled artifacts
    - Build directories are already ignored by .gitignore
    - Swift Package Manager .build/ directory is properly ignored
    - Prepares repository for clean open source release

--- build/DiceLang.build/Debug-iphoneos/DiceLang.build/Objects-normal/arm64/DiceLang_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/andrew.lenox@gc.com/Development/DiceLanguage/DiceLang/build/DiceLang.build/Debug-iphoneos/DiceLang.build/DerivedSources/DiceLang_vers.c
--- build/DiceLang.build/Release-iphoneos/DiceLang.build/Objects-normal/arm64/DiceLang_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/andrew.lenox@gc.com/Development/DiceLanguage/DiceLang/build/DiceLang.build/Release-iphoneos/DiceLang.build/DerivedSources/DiceLang_vers.c
